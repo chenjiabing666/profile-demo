7346f92d8dc24d764ec6c0eb89cd66f13affc081
chenjiabing666 <18796327106@163.com>
2020-03-24 22:40:11 +0800

    springBoot整合Maven多环境配置

---
+++ src/main/java/com/vivachek/cloud/profiledemo/ProfileDemoApplication.java
@@ -0,0 +1,13 @@
+package com.vivachek.cloud.profiledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfileDemoApplication.class, args);
+    }
+
+}
